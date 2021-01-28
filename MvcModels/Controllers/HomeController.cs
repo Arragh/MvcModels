a81dce64b1e076e2775c478595899ed4468d65f6
flashpoint1986@mail.ru <flashpoint1986@mail.ru>
2021-01-28 19:28:17 +0300

    Добавьте файлы проекта.

---
+++ MvcModels/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcModels.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+        public HomeController(IRepository repository) => this.repository = repository;
+
+        //public ViewResult Index(int id) => View(repository[id] ?? repository.People.Last());
+        public IActionResult Index(int? id)
+        {
+            Person person;
+            if (id.HasValue && (person = repository[id.Value]) != null)
+            {
+                return View(person);
+            }
+            return NotFound();
+        }
+    }
+}
